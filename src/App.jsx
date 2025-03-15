e8eab37ecd16f085630b9190f32e20bd87cdf6d6
Zohaibkhan2610 <zohaibkhan2610765@gmail.com>
2025-03-15 11:12:26 +0500

    v1.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import WomenShoe from "./Pages/Women/WomenShoe";
 import Header from "./Components/Header";
 import MenShoe from "./Pages/Men/MenShoe";
 import ScrollToTop from "./Components/ScrollToTop";
-import LoginPage from "./Pages/Registration/LoginPage";
-import ProfilePage from "./Pages/Registration/ProfilePage";
 import { AuthProvider } from "./Context/AuthContext";
-import RegistrationPage from "./Pages/Registration/RegisterPage";
+import RegistrationPage from "./Pages/Registartion/RegisterPage"; 
 import { WishlistProvider } from "./Context/WishlistContext";
+import LoginPage from "./Pages/Registartion/LoginPage";
+import ProfilePage from "./Pages/Registartion/ProfilePage";
 function App() {
   return (
     <WishlistProvider>
